037bd9b0d1271138e7b32e083e2c12db21648a6c
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-25 23:19:01 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS ShoutBox</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+	
+	<div id="container">
+		<header> 
+			<h1>JS Shoutbox</h1>
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+			<script type="text/javascript" src="js/script.js"></script>
+		</header>
+	</div>
+	<div id="shouts">
+		<ul>
+			<li></li>
+		</ul>
+	</div>
+		
+	</div>
+	<footer>
+		<label>Name: </label>
+		<input type="text" id="name" name="">
+		<label>Shout Text </label>
+		<input type="text" id="shout" name="">
+		<input type="submit" id="submit" name="" value="SHOUT!">
+	</footer>
+</body>
+</html>
---
+++ shoutbox.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS Shoutbox</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript"	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" scr="js/script.js"></script>
+</head>
+<body>
+	<header>
+		<h1>JS Shoutbox</h1>
+	</header>
+	<div id="shouts">
+		<ul>
+			<li></li>
+		</ul>
+	</div>
+	<footer>
+		<form>
+			<label>Name: </label>
+			<input type="text" id="name" name="">
+			<label>Shout Text" </label>
+			<input type="text" id="shout" name="">
+			<input type="submit" id="submit" value="SHOUT!" name="">
+		</form>
+	</footer>
+</body>
+</html>
