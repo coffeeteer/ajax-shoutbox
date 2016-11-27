a09943c5505359bbe534f423ffc69a8395abbc9f
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-26 20:19:05 -0500

    work mostly done need to find the problem with php server

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php include 'database.php' ?>
+
+<?php 
+	//Create query
+	$query = "Select + FROM shouts ORDER BY id DESC";
+	$shouts = mysqli_query($con, $query);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,7 +25,9 @@
 	</div>
 	<div id="shouts">
 		<ul>
-			<li></li>
+		<?php while ($row = mysqli_fetch_assoc($shouts)) : ?>
+			<li><?php echo $row['name']; ?> <?php echo $row['shout']; ?> <?php echo $row['date']; ?>;</li>
+		<?php  endwhile; ?>
 		</ul>
 	</div>
 		
@@ -32,3 +41,32 @@
 	</footer>
 </body>
 </html>
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title>JS Shoutbox</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript"	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" scr="js/script.js"></script>
+</head>
+<body>
+	<header>
+		<h1>JS Shoutbox</h1>
+	</header>
+	<div id="shouts">
+		<ul>
+			<li></li>
+		</ul>
+	</div>
+	<footer>
+		<form>
+			<label>Name: </label>
+			<input type="text" id="name" name="">
+			<label>Shout Text" </label>
+			<input type="text" id="shout" name="">
+			<input type="submit" id="submit" value="SHOUT!" name="">
+		</form>
+	</footer>
+</body>
+</html> -->
--- shoutbox.php
+++ shoutbox.php
@@ -1,28 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>JS Shoutbox</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script type="text/javascript"	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script type="text/javascript" scr="js/script.js"></script>
-</head>
-<body>
-	<header>
-		<h1>JS Shoutbox</h1>
-	</header>
-	<div id="shouts">
-		<ul>
-			<li></li>
-		</ul>
-	</div>
-	<footer>
-		<form>
-			<label>Name: </label>
-			<input type="text" id="name" name="">
-			<label>Shout Text" </label>
-			<input type="text" id="shout" name="">
-			<input type="submit" id="submit" value="SHOUT!" name="">
-		</form>
-	</footer>
-</body>
-</html>
+<?php include 'database.php'; ?>
+
+<?php 
+	if (isset($_POST['name']) && isset($_POST['shout'])) {
+		$name = myslqi_real_escape_string($con, $_POST['name']);
+		$shout = myslqi_real_escape_string($con, $_POST['shout']);
+		$date = myslqi_real_escape_string($con, $_POST['date']);
+
+		//Set Timezone
+		date_default_timezone_set('America/New_York');
+		$date = date('h:i:s a', time());
+
+		$query = 'INSERT INTO shouts (name, shout, date)
+		VALUES ('$name', '$shout', '$date')';
+
+		if (!mysqli_query($con, $query) ) {
+			echo 'Error: ' .mysqli_error($con);
+		} else {
+			echo '<li>' .$name.': '.$shout.' ['.$date.']</li>';
+		}
+	}
+?>
